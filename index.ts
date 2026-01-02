14df070d0d58487b9470468191feea0b6acfb4ea
kramakrishnan@cloudflare.com <rkarthik.iamok@gmail.com>
2026-01-02 12:14:55 -0500

    Added some basics

--- index.ts
+++ index.ts
@@ -1,4 +1,38 @@
-// Starting with basic Typescript
+// Benefits of Typescript
 
-const greeting: string = "Typescript says, 'Hello, World!'";
-console.log(greeting);
+// 1 - better error feedback
+
+function reverse(str: string) {
+  return str.split("").reverse().join("");
+}
+
+console.log(reverse("hello"));
+// console.log(reverse(123));
+
+// 2 - better autocompletion and code hints
+const reversed = reverse("ninja");
+
+// 3 - Custom Types
+// Interface - custom type definition
+// Interfaces is a virtual contract used only for type-checking
+// Implementation is purely architectural
+// Vanishes at run time
+
+interface MenuItem {
+  title: string;
+  cost: number;
+}
+
+function printMenuItem(item: MenuItem) {
+  console.log(item.title, ":", item.cost);
+}
+
+// Print the Menu Item
+
+printMenuItem({ title: "Pizza", cost: 10 });
+
+// Error Example 1
+// printMenuItem({ title: "Pizza", cost: "10" });
+
+// Error Example 3
+// printMenuItem({ name: "Pizza", cost: 10 });
